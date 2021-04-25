b84045dd7768ba466bbaaa786549a3cdde7db08c
Stig Andre <stigaro@stud.ntnu.no>
2021-04-25 18:23:32 +0200

    Added microphone based recognizer Recognizer is now a stm for easier access and event handling

--- recognizer.py
+++ recognizer.py
@@ -2,59 +2,82 @@ import speech_recognition as sr
 from stmpy import Machine, Driver
 
 
+# noinspection PyTypeChecker
+# noinspection PyBroadException
 class Recognizer:
-    def __init__(self, audio_file_name: str):
-        self.latest_recognition = ""
+    """
+    Class that represents a state machine which detects audio and recognizes it.
+    If audio was found, recognized AND was activated with the keyword 'recognition_keyword'
+    the state machine will send the recognition as argument to the observers in a transition.
+    """
+
+    def __init__(self, recognition_keyword='communicator', stm_observers=[]):
+        self.stm: Machine = None
+        self.stm_observers: list = stm_observers
+        self.microphone = sr.Microphone()
         self.speech_converter = sr.Recognizer()
-        self.audio_file_name = audio_file_name
-        self.stm = None
+        self.audio = None
+        self.recognition_word = recognition_keyword
+        self.recognition = ""
+
+    def update_observers(self):
+        # Prepares recognition. By default we only send the last activation keyword string
+        recognition_string = self.recognition.split(self.recognition_word)[-1].lstrip()
+        print("[OBSERVER_UPDATE]: '{}'".format(recognition_string))
+        for stm_observer in self.stm_observers:
+            try:
+                self.stm.driver.send('adressed', stm_observer, args={
+                    'recognition_word': self.recognition_word,
+                    'recognition_string': recognition_string
+                })
+            except Exception:
+                print("WARNING; Recognizer raised exception when sending to observer")
+                continue  # We ignore any exceptions.
+
+    def was_adressed(self):
+        if self.recognition_word in self.recognition:
+            print("[COMPOUND-TRUE]: Recognition keyword used")
+            self.update_observers()
+        else:
+            print("[COMPOUND-FALSE]: Recognition keyword not used")
+
+        return 'listening'
+
+    def listen(self):
+        print("\n\n[LISTENING]: Listening for audio")
+        with self.microphone as microphone:
+            self.speech_converter.adjust_for_ambient_noise(microphone)
+            self.audio = self.speech_converter.listen(microphone)
+        self.stm.send("new_audio")
 
     def recognize(self):
-        # noinspection PyBroadException
         try:
-            print("[RECOGNIZE]: Recognizing audio")
+            print('[RECOGNIZING]: Recognizing found audio')
+            self.recognition = self.speech_converter.recognize_google(self.audio).lower()
+            self.stm.send("recognized")
 
-            with sr.AudioFile(self.audio_file_name) as audio_source:
-                audio = self.speech_converter.listen(audio_source)
-            self.latest_recognition = "[MESSAGE]: " + self.speech_converter.recognize_google(audio)
-
-        except FileNotFoundError:
-            print('[RECOGNIZE]: Was unable to recognize')
-            self.latest_recognition = "[ERROR]: " + "Could not load audio file"
         except Exception:
-            print('[RECOGNIZE]: Was unable to recognize')
-            self.latest_recognition = "[ERROR]: " + "General internal error"
-
-        # Standardize to lowercase and return that we are done
-        self.latest_recognition = self.latest_recognition.lower()
-        self.stm.send('done')
-        return self.latest_recognition()
-
-    def report(self):
-        # TODO: Needs to send message, currently only prints latest recognition
-        print("Latest recognition => " + self.latest_recognition.lower())
-        pass
+            print('[RECOGNIZING]: Was unable to recognize audio')
+            self.recognition = ""
+            self.stm.send("recognition_error")
 
 
 if __name__ == "__main__":
-    recognizer = Recognizer('output.wav')
+    recognizer = Recognizer()
 
-    t_i0 = {'source': 'initial', 'target': 'ready'}
-    t_01 = {'trigger': 'recognize', 'source': 'ready', 'target': 'recognizing'}
-    t_10 = {'trigger': 'done', 'source': 'recognizing', 'target': 'ready'}
+    t_i0 = {'source': 'initial', 'target': 'listening'}
+    t_01 = {'trigger': 'new_audio', 'source': 'listening', 'target': 'recognizing'}
+    t_10_a = {'trigger': 'recognition_error', 'source': 'recognizing', 'target': 'listening'}
+    t_10_b = {'trigger': 'recognized', 'source': 'recognizing', 'function': recognizer.was_adressed}
 
-    s_ready = {'name': 'ready', 'report': 'report()'}
-    s_recognizing = {'name': 'recognizing', 'entry': 'recognize()', 'report': 'defer', 'recognize': 'defer'}
+    s_listening = {'name': 'listening', 'entry': 'listen'}
+    s_recognizing = {'name': 'recognizing', 'entry': 'recognize'}
 
-    stm = Machine(name='stm', transitions=[t_i0, t_01, t_10], states=[s_ready, s_recognizing], obj=recognizer)
+    stm = Machine(name='stm_recognizer', transitions=[t_i0, t_01, t_10_a, t_10_b], states=[s_listening, s_recognizing], obj=recognizer)
     recognizer.stm = stm
 
+    print("[DRIVER]: driver starting")
     driver = Driver()
     driver.add_machine(stm)
     driver.start()
 
-    print("[DRIVER]: driver started")
-
-    # TODO: Temporary code to test the state machine
-    driver.send('recognize', 'stm')
-    driver.send('report', 'stm')
