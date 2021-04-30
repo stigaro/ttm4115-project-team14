b915565f22cc4f5775d6fd1ab35e7f7982dba6d3
shirajuki <shirajuki00@gmail.com>
2021-04-30 14:23:42 +0200

    fix: some minor merge conflict fixes

--- client/patient_walkietalkie.py
+++ client/patient_walkietalkie.py
@@ -264,8 +264,6 @@ class WalkieTalkie:
         th = Thread(target=self.threaded_iterate, args=[self.lock, remove]);
         th.start()
         th.join()
-                os.rename(f"{queue_folder}/{filename}", f"{queue_folder}/{i}.wav")
-        self.update_led()
 
     # Request replay message from the server
     def get_latest_user_message(self):
@@ -300,16 +298,10 @@ class WalkieTalkie:
 
     def update_led(self, queue_pad = 0):
         if self.app != None:
-            if is_error:
-                self.app.setBgImage("images/bg_red.gif")
+            if self.check_message_queue(1+queue_pad): # check if there are more than 1 (default) messages in queue
+                self.app.setBgImage("images/bg_green.gif")
             else:
-                # Blink green if there's message in queue
-                queue_folder = "message_queue"
-                queue_length = len(os.listdir(queue_folder))
-                if self.check_message_queue(1+queue_pad): # check if there are more than 1 (default) messages in queue
-                    self.app.setBgImage("images/bg_green.gif")
-                else:
-                    self.app.setBgImage("images/bg.gif")
+                self.app.setBgImage("images/bg.gif")
 
     def check_queue(self):
         if self.check_message_queue(0): # check if there are more than 0 messages in queue
